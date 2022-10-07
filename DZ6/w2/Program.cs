1ec47191a1753e215c47b9fbc9f37e00262d92b6
Uber11222 <112197039+Uber11222@users.noreply.github.com>
2022-10-07 12:29:08 +0400

    DZ6

---
+++ DZ6/w2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Для вычисления точки пересечения y=k1*x+b1 и y=k2*x=b2");
+Console.WriteLine("введите значения b1 b2 k1 k2");
+double b1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+Console.WriteLine("Точка пересечения с координатами Х=" + x + "   Y=" + y);
